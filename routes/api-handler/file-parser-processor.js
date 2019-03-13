cd5799ecf58bd3b89ba663410b6cd4b98f00658e
Damanpreet Singh <daman.preetsingh@yahoo.co.in>
2019-03-13 23:14:57 +0530

    call rest apis for update preferences

--- routes/api-handler/file-parser-processor.js
+++ routes/api-handler/file-parser-processor.js
@@ -8,6 +8,7 @@ let uploadDir = __dirname + "/../../scrubFolder/";
 let excelProcessor = require('../../utils/excel-parser.js');
 let utils = require('../../utils/utils.js');
 let userProcessor = require('./user-processor.js');
+let request = require('../../server/http-request.js');
 
 /*Global Vars*/
 let logger = logManager.getLogger();
@@ -88,57 +89,117 @@ function processPreferencesFile(index, xlData, cb) {
         let phone = xlData[index]['phone'];
         logger.info('[processPreferencesFile]', phone);
 
+        let subscriberRequestObject = {
+            url: 'http://localhost:3000/subscriber/' + phone,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            json: true
+        };
 
-        if (chainManager.doesUserExist(phone)) {
+        subscriberRequestObject['method'] = 'GET';
 
-            let bulkUpdate = [
-                xlData[index]['uccInsurance'],
-                xlData[index]['uccRealstate'],
-                xlData[index]['uccEducation'],
-                xlData[index]['uccHealth'],
-                xlData[index]['uccGood'],
-                xlData[index]['uccEnt'],
-                xlData[index]['uccTourism'],
-                xlData[index]['uccFood'],
-                xlData[index]['mocVoice'],
-                xlData[index]['mocSMS'],
-                xlData[index]['mocADrec'],
-                xlData[index]['mocADlive'],
-                xlData[index]['mocRobo'],
-                xlData[index]['bandT1'],
-                xlData[index]['bandT2'],
-                xlData[index]['bandT3'],
-                xlData[index]['bandT4'],
-                xlData[index]['bandT5'],
-                xlData[index]['bandT6'],
-                xlData[index]['bandT7'],
-                xlData[index]['bandT8'],
-                xlData[index]['bandT9'],
-                xlData[index]['dayMon'],
-                xlData[index]['dayTue'],
-                xlData[index]['dayWed'],
-                xlData[index]['dayThus'],
-                xlData[index]['dayFri'],
-                xlData[index]['daySat'],
-                xlData[index]['daySun'],
-                xlData[index]['datNat']
-            ];
-            console.log(bulkUpdate);
+        request.fetchData(subscriberRequestObject, function (err, subscriberDetail) {
+            if (err) {
+                index++;
+                processPreferencesFile(index, xlData, cb);
+            } else {
+                let participant = {
+                    $class: 'org.example.biznet.subscriber',
+                    'uccInsurance': xlData[index]['uccInsurance'] === '1',
+                    'uccRealstate': xlData[index]['uccRealstate'] === '1',
+                    'uccEducation': xlData[index]['uccEducation'] === '1',
+                    'uccHealth': xlData[index]['uccHealth'] === '1',
+                    'uccGood': xlData[index]['uccGood'] === '1',
+                    'uccEnt': xlData[index]['uccEnt'] === '1',
+                    'uccTourism': xlData[index]['uccTourism'] === '1',
+                    'uccFood': xlData[index]['uccFood'] === '1',
+                    'mocVoice': xlData[index]['mocVoice'] === '1',
+                    'mocSMS': xlData[index]['mocSMS'] === '1',
+                    'mocADrec': xlData[index]['mocADrec'] === '1',
+                    'mocADlive': xlData[index]['mocADlive'] === '1',
+                    'mocRobo': xlData[index]['mocRobo'] === '1',
+                    'bandT1': xlData[index]['bandT1'] === '1',
+                    'bandT2': xlData[index]['bandT2'] === '1',
+                    'bandT3': xlData[index]['bandT3'] === '1',
+                    'bandT4': xlData[index]['bandT4'] === '1',
+                    'bandT5': xlData[index]['bandT5'] === '1',
+                    'bandT6': xlData[index]['bandT6'] === '1',
+                    'bandT7': xlData[index]['bandT7'] === '1',
+                    'bandT8': xlData[index]['bandT8'] === '1',
+                    'bandT9': xlData[index]['bandT9'] === '1',
+                    'dayMon': xlData[index]['dayMon'] === '1',
+                    'dayTue': xlData[index]['dayTue'] === '1',
+                    'dayWed': xlData[index]['dayWed'] === '1',
+                    'dayThus': xlData[index]['dayThus'] === '1',
+                    'dayFri': xlData[index]['dayFri'] === '1',
+                    'daySat': xlData[index]['daySat'] === '1',
+                    'daySun': xlData[index]['daySun'] === '1',
+                    'datNat': xlData[index]['datNat'] === '1',
+                    'consentnos': subscriberDetail['consentnos']
+                };
+                subscriberRequestObject['method'] = 'PUT';
+                subscriberRequestObject['body'] = participant;
+                request.fetchData(subscriberRequestObject, function (err, response) {
+                    index++;
+                    processPreferencesFile(index, xlData, cb);
+                })
 
-            let result = chainManager.bulkupdateUCC(phone, bulkUpdate);
-            logger.info('[processPreferencesFile] bulkupdateUCC result', result);
-
-            index++;
-            processPreferencesFile(index, xlData, cb);
-
-        } else {
-            index++;
-            processPreferencesFile(index, xlData, cb);
-        }
-
+            }
+        });
 
     } else {
         cb(null, true);
+    }
+}
+
+function test() {
+    let source = [["9876543210", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543211", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543212", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543213", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543214", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543215", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543216", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543217", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543218", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543219", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543220", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543221", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543222", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543223", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543224", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543225", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543226", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543227", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543228", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543229", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543230", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543231", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543232", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543233", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543234", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543235", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543236", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543237", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543238", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543239", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543240", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"], ["9876543241", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0", "0", "1", "0", "0", "0"]];
+
+    for (let i = 0; i < source.length; i++) {
+        let value = source[i];
+        let participant = {
+            $class: 'org.example.biznet.subscriber',
+            'uccInsurance': value[1] === 1,
+            'uccRealstate': value[2] === 1,
+            'uccEducation': value[3] === 1,
+            'uccHealth': value[4] === 1,
+            'uccGood': value[5] === 1,
+            'uccEnt': value[6] === 1,
+            'uccTourism': value[7] === 1,
+            'uccFood': value[8] === 1,
+            'mocVoice': value[9] === 1,
+            'mocSMS': value[10] === 1,
+            'mocADrec': value[11] === 1,
+            'mocADlive': value[12] === 1,
+            'mocRobo': value[13] === 1,
+            'bandT1': value[14] === 1,
+            'bandT2': value[15] === 1,
+            'bandT3': value[16] === 1,
+            'bandT4': value[17] === 1,
+            'bandT5': value[18] === 1,
+            'bandT6': value[19] === 1,
+            'bandT7': value[20] === 1,
+            'bandT8': value[21] === 1,
+            'bandT9': value[22] === 1,
+            'dayMon': value[23] === 1,
+            'dayTue': value[24] === 1,
+            'dayWed': value[25] === 1,
+            'dayThus': value[26] === 1,
+            'dayFri': value[27] === 1,
+            'daySat': value[28] === 1,
+            'daySun': value[29] === 1,
+            'datNat': value[30] === 1,
+            'consentnos': ''
+        };
+
+        servicesubscriber.updateParticipant(value[0], participant)
+            .toPromise()
+            .then(() => {
+                //logic
+            })
+
+
     }
 }
 
---
+++ server/http-request.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/** @module server/lib/httpRequest */
+
+/** Modules import */
+let request = require('request');
+
+/** Local Imports */
+let logManager = require('./../utils/log-manager.js');
+
+/** Global Vars */
+let logger = logManager.getLogger();
+
+/** Function to handle the http request
+ * @param {string} url - refers to the url for which http request needs to be made
+ * @param {function} cb - refers to the callback function to which http request result needs to be passed
+ */
+function makeRequest(url, cb) {
+    logger.info('[http-requests] Making http request to using the url ', url);
+
+    let options = {};
+    if (typeof url === 'object') {
+        options = url;
+        url = options.url;
+    }
+
+    /** If no url has been passed return */
+    if (!url) {
+        return;
+    }
+    logger.info('[data] network call BEGIN. [url: %s]', url);
+
+    let currTime = (new Date()).getTime();
+    /** Recording current time for performance */
+
+    request({
+        uri: url,
+        method: options.method || 'GET', /** By default pick the method GET */
+        timeout: options.timeout || 10000, /** Request timeout set to be 10,000 ms */
+        headers: options.headers || {},
+        qs: options.qs || null,
+        json: typeof options.json === 'boolean' ? options.json : true,
+        followRedirect: true,
+        maxRedirects: 10, /** Max redirects set to 10 */
+        body: options.body || null
+    }, function (error, response, body) {
+        logger.info('[http-request] network call DONE. [url: %s][%s ms]', url, (new Date()).getTime() - currTime);
+        return cb(error, body);
+    });
+}
+
+/**
+ * Fetches data from a api endpoint
+ * @param {string|object} url - url to fetch data from. If object, must contain "url" field
+ * @param {function} cb - callback to call when done
+ */
+exports.fetchData = function (url, cb) {
+    makeRequest(url, cb);
+};
